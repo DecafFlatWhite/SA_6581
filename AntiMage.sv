b2435b4fe4bdf5e8c7ac1ef44f8646e269cf8061
DecafFlatWhite <155454729+DecafFlatWhite@users.noreply.github.com>
2024-12-28 02:59:26 -0800

    AM is now working

--- AntiMage.sv
+++ AntiMage.sv
@@ -1,21 +1,20 @@
-// This module is basically the same as the amplitude modulator in:
-// https://github.com/gundy/tiny-synth
 
 module am(
     input clk,
     input [7:0] envelope,
-    input signed [11:0] waveIn,
-    output wire signed [11:0] waveOut
+    input [11:0] waveIn,
+    output reg [11:0] waveOut
 );
 
-    wire signed [7:0] envelopeSigned;
-    reg signed [19:0] waveScaled;
-    assign envelopeSigned = {1'b0, envelope[7:0]};
+    reg [19:0] waveScaled;
 
     always_ff @ (posedge clk) begin
-        waveScaled <= (waveIn * envelopeSigned);
+        waveScaled = (waveIn * envelope);
+        // The value need to be divided by 256 to normalize the envelope, which
+        // is the same as right shift by 8 bits. 
+        waveOut <= (waveScaled >> 8); 
     end
 
-    assign waveOut = waveScaled[19:8];
-
 endmodule
+
+
