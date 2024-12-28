ec81a74118d9a4d1be7adafa4402283dff1d02f3
DecafFlatWhite <155454729+DecafFlatWhite@users.noreply.github.com>
2024-12-28 07:23:24 -0800

    update: AM was not working, now things were fixed

--- AntiMage.sv
+++ AntiMage.sv
@@ -2,17 +2,20 @@
 module am(
     input clk,
     input [7:0] envelope,
-    input [11:0] waveIn,
+    input signed [11:0] waveIn,
     output reg [11:0] waveOut
 );
-
-    reg [19:0] waveScaled;
-
+    reg signed [8:0] signedEnvelope;
+    reg signed [11:0] signedWaveIn;
+    reg signed [19:0] waveScaled;
+    
     always_ff @ (posedge clk) begin
-        waveScaled = (waveIn * envelope);
+        signedEnvelope = {1'b0, envelope};
+        signedWaveIn = waveIn - 12'd2048;
+        waveScaled = (signedWaveIn * signedEnvelope);
         // The value need to be divided by 256 to normalize the envelope, which
         // is the same as right shift by 8 bits. 
-        waveOut <= (waveScaled >> 8); 
+        waveOut <= ((waveScaled >> 8) + 12'd2048); 
     end
 
 endmodule
